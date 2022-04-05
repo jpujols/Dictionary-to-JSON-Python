812092597b4940d730004adea4cc5393fdb5fbc7
Joaquin Pujols <54540355+jpujols@users.noreply.github.com>
2022-04-05 14:40:02 +0000

    add/update commands to update JSON file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
- 
+
+#Saving dictionary in a JSON file
 d = {"employees":[{"firstName": "John", "lastName": "Doe"},
                 {"firstName": "Anna", "lastName": "Smith"},
                 {"firstName": "Peter", "lastName": "Jones"}],
@@ -8,4 +9,13 @@ d = {"employees":[{"firstName": "John", "lastName": "Doe"},
  
 with open("company1.json", "w") as file:
     json.dump(d, file, indent=4)
+print("Dict >> JSON Completed")
 
+#Add/Update JSON File
+with open("company1.json", "r+") as file:
+  d = json.loads(file.read())
+  d["employees"].append(dict(firstName = "Albert", lastName = "Toto"))
+  file.seek(0)
+  json.dump(d, file, indent=4, sort_keys=True)
+  file.truncate()
+print("JSON updated")
